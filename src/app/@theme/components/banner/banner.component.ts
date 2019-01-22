26e91550ca5f9c67c1f31175aaa8eca5240c76b4
Vladislav Ahmetvaliev <ahmetvaliev.vladislav@gmail.com>
2019-01-22 12:43:11 +0300

    feat: add producthunt banner (#45)

--- src/app/@theme/components/banner/banner.component.ts
+++ src/app/@theme/components/banner/banner.component.ts
@@ -7,24 +7,28 @@
 import { Component, HostBinding, Inject, OnInit } from '@angular/core';
 import { NB_WINDOW } from '@nebular/theme';
 
-const HIDE_BANNER_KEY = 'HIDE_RELEASE_2_BANNER';
+const HIDE_BANNER_KEY = 'HIDE_PRODUCT_HUNT_BANNER';
 
 @Component({
   selector: 'eva-release-banner',
   template: `
     <div class="heading-with-icon">
-      <img class="icon" src="assets/img/bell-white.svg" alt="bell">
-      <h1 class="banner-heading">Nebular 3.0 stable <br> with 30+ components is out!</h1>
+      <img class="icon" src="/assets/img/product-hunt-cat.png" alt="Product Hunt">
+      <div class="banner-content">
+        <h2 class="banner-heading">ngx-admin is on Product Hunt today!</h2>
+        <p class="cta">Please
+          <a class="cta-link"
+             href="https://www.producthunt.com/posts/ngx-admin"
+             target="_blank">
+            share
+          </a>
+          your feedback :)
+        </p>
+      </div>
       <button class="close-button" aria-label="close" (click)="closeBanner()">
         <i [innerHTML]="'close' | eva: { width: 24, height: 24, fill: '#ffffff' }"></i>
       </button>
     </div>
-    <p class="cta">
-      Don't forget to <a class="cta-link"
-                         href="https://akveo.github.io/nebular?utm_source=eva-icons&utm_medium=banner_link"
-                         target="_blank">
-      check out</a> and star our repo :)
-    </p>
   `,
   styleUrls: ['./banner.component.scss'],
 })
@@ -33,7 +37,7 @@ export class BannerComponent implements OnInit {
   storage: Storage;
 
   @HostBinding('attr.hidden')
-  isHidden: boolean | null = null;
+  isHidden: true | null = null;
 
   @HostBinding('attr.dir')
   dir = 'ltr';
@@ -43,11 +47,8 @@ export class BannerComponent implements OnInit {
 
   ngOnInit() {
     this.storage = this.window.localStorage;
-    this.isHidden = this.isBannerKeyInLocalStorage();
-  }
 
-  isBannerKeyInLocalStorage(): boolean | null {
-    return this.storage && this.storage.getItem(HIDE_BANNER_KEY)
+    this.isHidden = this.storage && this.storage.getItem(HIDE_BANNER_KEY)
       ? true
       : null;
   }
@@ -56,7 +57,6 @@ export class BannerComponent implements OnInit {
     if (this.storage) {
       this.storage.setItem(HIDE_BANNER_KEY, 'true');
     }
-
     this.isHidden = true;
   }
 }
